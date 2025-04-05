30c8cb472413b3a1dd29aec9f2813e5ada477873
Prudvi Satya Teja Bachala <prudvisatyateja1234@gmail.com>
2025-04-05 09:48:53 +0530

    add the tailwind css to the login and signup pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 const Login = () => {
     return (
-        <div>
-            <div className="login-container">
-                <input type="text" id="username" placeholder="username"></input>
-                <br></br>
-                <input type="text" id="password" placeholder="password"></input>
-                <button className="Login">Login</button>
+        <div className="bg-green-100 mx-auto w-80 mt-20 p-5">
+            <div className=" mx-25 font-bold text-2xl">Login</div>
+            <div className="flex flex-wrap justify-around p-10">
+                <input
+                    className="border-2 p-2 my-2"
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                ></input>
+                <input
+                    className="border-2 p-2 my-4"
+                    type="password"
+                    id="password"
+                    se
+                    placeholder="Password"
+                ></input>
+                <button className="bg-orange-400 textlg font-semibold w-50 h-10 rounded-xl my-4">
+                    Login
+                </button>
             </div>
         </div>
     );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,24 @@
 const Signup = () => {
     return (
-        <div className="signup-container">
-            <input type="text" id="username" placeholder="username"></input>
-            <br></br>
-            <input type="text" id="password" placeholder="password"></input>
-            <button className="Signup">Signup</button>
+        <div className="bg-green-100 mx-auto w-80 mt-20 p-5">
+            <div className=" mx-22 font-bold text-2xl">SignUp</div>
+            <div className="flex flex-wrap justify-around p-10">
+                <input
+                    className="border-2 p-2 my-2"
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                ></input>
+                <input
+                    className="border-2 p-2 my-4"
+                    type="text"
+                    id="password"
+                    placeholder="Password"
+                ></input>
+                <button className="bg-orange-400 textlg font-semibold w-50 h-10 rounded-xl my-4">
+                    Signup
+                </button>
+            </div>
         </div>
     );
 };
